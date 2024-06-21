82f6bb30c258fe2acfd2db31717d4f0392ab34a9
Ethan Chua <74781215+artistmonky@users.noreply.github.com>
2024-06-21 16:14:45 -0500

    Update smbus2 Test Code after testing on ARK Jetson

--- PCB Test Code/smbus2 Test Code.py
+++ PCB Test Code/smbus2 Test Code.py
@@ -6,18 +6,18 @@ import keyboard
 DEVICE_ADDRESS = 0x64
 
 # Create a smbus object
-bus = smbus2.SMBus(0)  # 0 indicates /dev/i2c-0
+bus = smbus2.SMBus(1)  # 1 indicates /dev/i2c-1
 
-# set bus to default high. writing high to DAC register and EEPROM
+# Set DAC output to default high by writing high to DAC register and DAC EEPROM
 message = [0x00, 0x00, 0x60, 0x00, 0x00]
 bus.write_i2c_block_data(DEVICE_ADDRESS, 0x60, message)
 print("Circuit is open by default.")
 
-# while loop to set high / low when there's an input, and escape key
+# Loop to set high / low when there's an input, and escape key
 while True:
     if keyboard.is_pressed('h'):  # for high output, writing only to DAC register
         message = [0xEC, 0x09, 0xEC]
-        bus.write_i2c_block_data(DEVICE_ADDRESS, 0x09, message)
+        bus.write_i2c_block_data(DEVICE_ADDRESS, 0x09, message) # note: the exact value (0x9EC) of high output was chosen arbitrarily.
         print("Circuit is now closed.")
 
     elif keyboard.is_pressed('l'): # for low output, writing only to DAC register
@@ -25,9 +25,9 @@ while True:
         bus.write_i2c_block_data(DEVICE_ADDRESS, 0x00, message)
         print("Circuit is now open.")
 
-    elif keyboard.is_pressed('e'):
+    elif keyboard.is_pressed('e'): # escape 
         print("Exiting program...")
-        break  # escape 
+        break
 
 # Close the bus connection
 bus.close()
